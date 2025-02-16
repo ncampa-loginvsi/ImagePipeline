c1e96ad029a4185a2fd9cf41c2134d50ddffd26c
Nick <99031249+ncampa-loginvsi@users.noreply.github.com>
2025-02-15 23:35:26 -0500

    Update InstallScript.ps1

--- InstallScript.ps1
+++ InstallScript.ps1
@@ -1,4 +1,16 @@
-﻿# Check if Chocolatey is installed, and install if not
+﻿function Write-LabsLogs {
+    param (
+        [string]$message
+    )
+    
+    $logFile = "C:\Labs\Install.log"
+    $currentDateTime = Get-Date -Format "MM-dd-yy-THH:mm:ss"
+    $logMessage = "$currentDateTime - Log: $Message"
+    Add-Content -Path $logFile -Value $logMessage
+}
+
+
+# Check if Chocolatey is installed, and install if not
 if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Write-Host "Chocolatey not found, installing..."
     Set-ExecutionPolicy Bypass -Scope Process -Force; 
@@ -6,33 +18,43 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
 }
 
 # Update Chocolatey to make sure it's the latest version
-choco upgrade chocolatey -y
+choco upgrade chocolatey -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install Google Chrome
-choco install googlechrome -y
+choco install googlechrome -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install Notepad++
-choco install notepadplusplus.install -y
+choco install notepadplusplus.install -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install Visual Studio Code
-choco install vscode -y
+choco install vscode -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install 7zip (popular file compression tool)
-choco install 7zip.install -y
+choco install 7zip.install -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install VLC Media Player (for media playback)
-choco install vlc -y
+choco install vlc -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install Firefox (for an alternative browser)
-choco install firefox -y
+choco install firefox -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install Node.js (for JavaScript/TypeScript development)
-choco install nodejs-lts -y
+choco install nodejs-lts -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Install LibreOffice (for office productivity suite)
-choco install libreoffice-fresh -y
+choco install libreoffice-fresh -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 # Optionally, you can clear the cache after installations to save space
-choco clean -y
+choco clean -y 2>&1  # Capturing standard and error output
+$chocoOutput | ForEach-Object { Write-LabsLogs $_ }
 
 Write-Host "All applications have been installed"
